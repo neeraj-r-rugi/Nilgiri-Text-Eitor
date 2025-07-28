c8d91ffe13e40a5a43c92606eea89fd228d4fdc6
Neeraj R Rugi <texonace123@gmail.com>
2025-07-28 17:57:01 +0530

    Added License to source File

--- src/main.c
+++ src/main.c
@@ -1,5 +1,20 @@
-#include "defines.h"
-//Test Comment
+/*
+Nilgiri Text Editor: A simple lightweight text/code editor developed as a student project!  
+Copyright (C) 2024 NEERAJ R RUGI
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 /*
     @author: Neeraj R Rugi
     @category: Code/Text Editor
@@ -13,6 +28,8 @@
         -> Comments bearing "!External Requirement" indicate header files not present in the standard C Library
 
 */
+#include "defines.h"
+
 
 // Global Variables
 GtkWidget * save_at_quit_popup = NULL;
